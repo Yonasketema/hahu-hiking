d44353a4dc8ddb8f42dc0c6ded06f5d79a41808e
yonasketema <yonask.kitaw@gmail.com>
2023-03-15 21:52:50 +0300

    add card component

---
+++ hahu/app/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+
+import Text from "./Text";
+import colors from "../config/colors";
+
+function Card({ title, subTitle, image }) {
+  return <View style={styles.card}></View>;
+}
+
+const styles = StyleSheet.create({});
+
+export default Card;
