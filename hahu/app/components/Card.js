1a1cdf63d5c37d003211225ec122158fe9b77ede
yonasketema <yonask.kitaw@gmail.com>
2023-03-16 11:09:32 +0300

    add card detail

--- hahu/app/components/Card.js
+++ hahu/app/components/Card.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { View, StyleSheet, Image } from "react-native";
 
-import Text from "./Text";
-import colors from "../config/colors";
-
 function Card({ title, subTitle, image }) {
-  return <View style={styles.card}></View>;
+  return (
+    <View style={styles.card}>
+      <Image style={styles.img} />
+      <View style={styles.description}>
+        <Text>title</Text>
+        <Text>location</Text>
+      </View>
+
+      <View style={styles.ratebox}>
+        <Text>4.8</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
