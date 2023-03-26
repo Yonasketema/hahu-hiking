fbdab9f55681264c40bc573486b890cf5c3e0cb1
yonasketema <yonask.kitaw@gmail.com>
2023-03-26 21:34:09 +0300

    add `DetailPage.js`

---
+++ hahu/app/screens/DetailPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+function DetailPage(props) {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
+export default DetailPage;
