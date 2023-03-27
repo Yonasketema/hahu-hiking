b3303c92b9db7caeab5645162a96a0c0e260837f
yonasketema <yonask.kitaw@gmail.com>
2023-03-27 21:45:55 +0300

    add first style

--- hahu/app/screens/HomeScreen.js
+++ hahu/app/screens/HomeScreen.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Card from "../components/Card";
+import Screen from "../components/Screen";
 
 function HomeScreen() {
-  return <View></View>;
+  return (
+    <Screen>
+      <Text style={styles.title}>plan your weekly Hiking</Text>
+      <Card />
+
+      <Text style={styles.title}>Trip</Text>
+
+      <View style={styles.trip_container}>
+        <Card style={{ width: "48%", height: 220 }} />
+        <Card style={{ width: "48%", height: 220 }} />
+      </View>
+
+      <View style={styles.trip_container}>
+        <Card style={{ width: "48%", height: 220 }} />
+        <Card style={{ width: "48%", height: 220 }} />
+      </View>
+    </Screen>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 23,
+    fontWeight: "bold",
+    textTransform: "capitalize",
+  },
+  trip_container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  home_screen: {
+    backgroundColor: "#f8f4f8",
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
