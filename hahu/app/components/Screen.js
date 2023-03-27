72c82dae8391bc252e6adcab907ce718db964cb2
yonasketema <yonask.kitaw@gmail.com>
2023-03-27 21:43:30 +0300

    add Screen component

---
+++ hahu/app/components/Screen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+
+function Screen({ children }) {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "#fcf2f1",
+    padding: 10,
+    flex: 1,
+  },
+});
+export default Screen;
