ca2ef33350d17c9acd27d4e3dd3f15b2dfa6a49e
yonasketema <yonask.kitaw@gmail.com>
2023-03-28 17:56:18 +0300

    refactor: trip list

--- hahu/app/screens/HomeScreen.js
+++ hahu/app/screens/HomeScreen.js
@@ -1,12 +1,55 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
 
 import { Foundation } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { AntDesign } from "@expo/vector-icons";
 import SmallCard from "../components/SmallCard";
+
+const list = [
+  {
+    rate: 4.5,
+    title: "Nature, Wildlife and Cave Church Tour",
+    subTitle: "731$",
+    img: "https://www.fodors.com/wp-content/uploads/2018/08/Ethiopia-Wildlife-Hero.jpg",
+  },
+  {
+    rate: 4.8,
+    title: "3 Days Omo Valley Tour",
+    subTitle: "988$",
+    img: "https://d2g6byanrj0o4m.cloudfront.net/images/49031/ethiopian_children.jpg",
+  },
+  {
+    rate: 4.6,
+    title: "Full Day Tour of Addis Ababa",
+    subTitle: "432$",
+
+    img: "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/bc/5a/0a.jpg",
+  },
+  {
+    rate: 4.9,
+    title: "Lalibela 3 Days/ 2 Nights",
+    subTitle: "999$",
+    img: "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcT26oCRGUfr9aaKIqcAHs1K6czjMHNb3f1d8e_2cCT66twqAVd7enzUnK2EKTmaJXn9",
+  },
+  {
+    rate: 4.7,
+    title: "Afar Danakil Depression",
+    subTitle: "$888",
+    img: "https://cdn.tourradar.com/s3/tour/1500x800/181291_4096a6f4.jpg",
+  },
+];
 
 function HomeScreen() {
   return (
@@ -42,13 +85,13 @@ function HomeScreen() {
           </View>
 
           <View style={styles.box_catagory}>
-            <Foundation name="mountains" size={27} color="#3777" />
-            <Text style={styles.text_catagory}>mountains</Text>
+            <MaterialCommunityIcons name="forest" size={27} color="#3777" />
+            <Text style={styles.text_catagory}>forest</Text>
           </View>
 
           <View style={styles.box_catagory}>
-            <Foundation name="mountains" size={27} color="#3777" />
-            <Text style={styles.text_catagory}>mountains</Text>
+            <MaterialIcons name="location-city" size={27} color="#3777" />
+            <Text style={styles.text_catagory}>Citys</Text>
           </View>
         </ScrollView>
       </View>
@@ -64,6 +107,9 @@ function HomeScreen() {
           <View style={{ marginRight: 10 }}>
             <Card />
           </View>
+          <View style={{ marginRight: 10 }}>
+            <Card />
+          </View>
 
           <View>
             <Card />
@@ -73,15 +119,19 @@ function HomeScreen() {
 
       <Text style={styles.title}>Trip</Text>
 
-      <View style={styles.trip_container}>
-        <SmallCard />
-        <SmallCard />
-      </View>
-
-      <View style={styles.trip_container}>
-        <SmallCard />
-        <SmallCard />
-      </View>
+      <FlatList
+        numColumns={2}
+        data={list}
+        keyExtractor={(item) => item.rate}
+        renderItem={({ item }) => (
+          <SmallCard
+            title={item.title}
+            subTitle={item.subTitle}
+            imgurl={item.img}
+            rating={item.rate}
+          />
+        )}
+      />
     </Screen>
   );
 }
@@ -91,9 +141,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 12,
-    justifyContent: "space-between",
     backgroundColor: "#fff",
-    width: 130,
+    maxWidth: 130,
+    gap: 9,
     height: 35,
     marginRight: 12,
   },
