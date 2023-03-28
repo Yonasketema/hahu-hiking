735d36416cc5b9c9bf04635110f5874195b443a3
yonasketema <yonask.kitaw@gmail.com>
2023-03-28 15:09:39 +0300

    refactor: trip card -> smallCard

--- hahu/app/screens/HomeScreen.js
+++ hahu/app/screens/HomeScreen.js
@@ -1,38 +1,109 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
+
+import { Foundation } from "@expo/vector-icons";
+
+import { AntDesign } from "@expo/vector-icons";
+import SmallCard from "../components/SmallCard";
 
 function HomeScreen() {
   return (
     <Screen>
       <Text style={styles.title}>plan your weekly Hiking</Text>
-      <Card />
+      <View
+        style={{
+          flexDirection: "row",
+          width: "100%",
+
+          backgroundColor: "#fff",
+          padding: 12,
+        }}
+      >
+        <AntDesign name="search1" size={24} color="#121" />
+        <TextInput
+          placeholder="Search destination"
+          style={{ marginHorizontal: 10 }}
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          marginVertical: 14,
+          width: "100%",
+        }}
+      >
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <View style={styles.box_catagory}>
+            <Foundation name="mountains" size={27} color="#3777" />
+            <Text style={styles.text_catagory}>mountains</Text>
+          </View>
+
+          <View style={styles.box_catagory}>
+            <Foundation name="mountains" size={27} color="#3777" />
+            <Text style={styles.text_catagory}>mountains</Text>
+          </View>
+
+          <View style={styles.box_catagory}>
+            <Foundation name="mountains" size={27} color="#3777" />
+            <Text style={styles.text_catagory}>mountains</Text>
+          </View>
+        </ScrollView>
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          marginBottom: 14,
+          width: "100%",
+        }}
+      >
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <View style={{ marginRight: 10 }}>
+            <Card />
+          </View>
+
+          <View>
+            <Card />
+          </View>
+        </ScrollView>
+      </View>
 
       <Text style={styles.title}>Trip</Text>
 
       <View style={styles.trip_container}>
-        <Card
-          style={{ width: "48%", height: 220, padding: 7, borderRadius: 16 }}
-        />
-        <Card
-          style={{ width: "48%", height: 220, padding: 7, borderRadius: 16 }}
-        />
+        <SmallCard />
+        <SmallCard />
       </View>
 
       <View style={styles.trip_container}>
-        <Card
-          style={{ width: "48%", height: 220, padding: 7, borderRadius: 16 }}
-        />
-        <Card
-          style={{ width: "48%", height: 220, padding: 7, borderRadius: 16 }}
-        />
+        <SmallCard />
+        <SmallCard />
       </View>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
+  box_catagory: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 12,
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    width: 130,
+    height: 35,
+    marginRight: 12,
+  },
+  text_catagory: {
+    fontSize: 15,
+    fontWeight: "600",
+    textTransform: "capitalize",
+    color: "#444",
+  },
+
   title: {
     fontSize: 23,
     fontWeight: "bold",
@@ -42,6 +113,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     marginBottom: 10,
+    width: "100%",
   },
   home_screen: {
     backgroundColor: "#f8f4f8",
