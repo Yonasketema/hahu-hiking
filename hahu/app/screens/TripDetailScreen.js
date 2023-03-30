a85f683da3300ade4691c112ce5a9755c351d794
yonasketema <yonask.kitaw@gmail.com>
2023-03-30 19:38:33 +0300

    add main img

--- hahu/app/screens/TripDetailScreen.js
+++ hahu/app/screens/TripDetailScreen.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+
+import { StyleSheet, View, Image, Text, StatusBar } from "react-native";
+
+import Screen from "../components/Screen";
 
 function TripDetailScreen(props) {
-  return <View></View>;
+  return (
+    <Screen>
+      <StatusBar translucent backgroundColor="rgba(0,0,0,.4)" />
+
+      <Image
+        style={styles.image}
+        source={{
+          uri: "https://d2g6byanrj0o4m.cloudfront.net/images/49031/ethiopian_children.jpg",
+        }}
+      ></Image>
+    </Screen>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: "55%",
+  },
+});
 export default TripDetailScreen;
