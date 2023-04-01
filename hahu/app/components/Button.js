45162c0a4bcec9e393ef555bdb73fccb8d1d8c14
yonasketema <yonask.kitaw@gmail.com>
2023-04-01 21:35:15 +0300

    add `Button` component

---
+++ hahu/app/components/Button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+function AppButton({ title, onPress, color = "blue" }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: color }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#121",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+    elevation: 7,
+  },
+  text: {
+    color: "#FFF",
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
+
+export default AppButton;
