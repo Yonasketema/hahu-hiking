17ce3064b372511930e4222ee37f4d3380554ed9
yonasketema <yonask.kitaw@gmail.com>
2023-04-03 21:57:54 +0300

    add booking detail screen

---
+++ hahu/app/screens/BookingDetailScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function BookingDetailScreen(props) {
+  return (
+    <View>
+      <Text>Detail Booking</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default BookingDetailScreen;
