61114452b27ac7d47ed0dc0849815594596d6b9c
yonasketema <yonask.kitaw@gmail.com>
2023-04-05 21:39:54 +0300

    refactor: text style

--- hahu/app/components/Check.js
+++ hahu/app/components/Check.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 function Check({ title }) {
@@ -12,11 +12,18 @@ function Check({ title }) {
       }}
     >
       <AntDesign name="check" size={16} color="#1116" />
-      <Text style={{ fontSize: 18, fontWeight: "600", color: "#3339" }}>
-        {title}
-      </Text>
+      <Text style={styles.title}>{title}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#3339",
+    width: 140,
+  },
+});
 
 export default Check;
