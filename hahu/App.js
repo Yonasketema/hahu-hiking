37c35306ef306d9d7e0ecf5f05812b4c54385e1c
yonasketema <yonask.kitaw@gmail.com>
2023-04-24 21:30:44 +0300

    add navBar

--- hahu/App.js
+++ hahu/App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+
+import {
+  NavigationContainer,
+  getFocusedRouteNameFromRoute,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import HomeScreen from "./app/screens/HomeScreen";
+import TripDetailScreen from "./app/screens/TripDetailScreen";
+import BookingDetailScreen from "./app/screens/BookingDetailScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <Stack.Screen name="TripDetail" component={TripDetailScreen} />
+    <Stack.Screen name="bookingDetail" component={BookingDetailScreen} />
+  </Stack.Navigator>
+);
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveBackgroundColor: "tomato",
+        tabBarActiveTintColor: "#fff",
+        tabBarInactiveBackgroundColor: "#eee",
+        tabBarInactiveTintColor: "#000",
+
+        // tabBarStyle: { display: "none" },
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="Booked" component={TripDetailScreen} />
+      <Tab.Screen
+        name="HomeBot"
+        component={StackNavigator}
+        options={({ route }) => ({
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+          tabBarStyle: ((route) => {
+            const routeName = getFocusedRouteNameFromRoute(route) ?? "";
+            console.log(routeName);
+            if (routeName === "TripDetail" || routeName === "bookingDetail") {
+              return { display: "none" };
+            }
+            return;
+          })(route),
+        })}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hahu App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <StackNavigator />
+       */}
+      <TabNavigator />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f8f4f8",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
