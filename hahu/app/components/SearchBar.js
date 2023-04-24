58724eef00890a927d2a81bcde9035a6d0321110
yonasketema <yonask.kitaw@gmail.com>
2023-04-24 12:00:57 +0300

    refactor: style prop  with stylesheet

--- hahu/app/components/SearchBar.js
+++ hahu/app/components/SearchBar.js
@@ -1,26 +1,28 @@
 import React from "react";
 
 import { AntDesign } from "@expo/vector-icons";
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 function SearchBar(props) {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        width: "90%",
-        backgroundColor: "#fff",
-        padding: 12,
-        marginHorizontal: "5%",
-      }}
-    >
+    <View style={styles.container}>
       <AntDesign name="search1" size={24} color="#121" />
-      <TextInput
-        placeholder="Search destination"
-        style={{ marginHorizontal: 10 }}
-      />
+      <TextInput placeholder="Search destination" style={styles.input_field} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    width: "92%",
+    backgroundColor: "#fff",
+    padding: 12,
+    marginHorizontal: "5%",
+  },
+  input_field: {
+    marginHorizontal: 10,
+  },
+});
 
 export default SearchBar;
