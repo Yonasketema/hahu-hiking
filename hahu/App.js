58c607f459838a72d5b1b18dd1f8ddcbd754c985
yonasketema <yonask.kitaw@gmail.com>
2023-04-25 08:18:20 +0300

    style : transparent navbar | add icon | add navigation action

--- hahu/App.js
+++ hahu/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 
 import {
   NavigationContainer,
@@ -7,28 +7,69 @@ import {
 } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useNavigation } from "@react-navigation/native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "./app/screens/HomeScreen";
 import TripDetailScreen from "./app/screens/TripDetailScreen";
 import BookingDetailScreen from "./app/screens/BookingDetailScreen";
 
+import { StackActions } from "@react-navigation/native";
+
+const popAction = StackActions.pop(1);
+
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{
-        headerShown: false,
+const StackNavigator = () => {
+  const navigate = useNavigation();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        title: "",
+        headerTransparent: true,
+        headerShadowVisible: false,
+
+        headerBackVisible: false,
+        headerTitleAlign: "center",
+
+        headerLeft: () => (
+          <TouchableWithoutFeedback
+            onPress={() => navigate.dispatch(popAction)}
+          >
+            <AntDesign
+              name="arrowleft"
+              size={24}
+              color="#333"
+              style={{ backgroundColor: "#eee", padding: 4, borderRadius: 50 }}
+            />
+          </TouchableWithoutFeedback>
+        ),
+        headerRight: () => (
+          <Ionicons
+            name="ios-notifications-outline"
+            size={24}
+            color="black"
+            style={{ backgroundColor: "#eee", padding: 4, borderRadius: 50 }}
+          />
+        ),
       }}
-    />
-    <Stack.Screen name="TripDetail" component={TripDetailScreen} />
-    <Stack.Screen name="bookingDetail" component={BookingDetailScreen} />
-  </Stack.Navigator>
-);
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name="TripDetail" component={TripDetailScreen} />
+      <Stack.Screen name="bookingDetail" component={BookingDetailScreen} />
+    </Stack.Navigator>
+  );
+};
 
 const TabNavigator = () => {
   return (
