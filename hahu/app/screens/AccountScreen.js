6dddb54253bba4aa400a37bf0f2ecc37ff793852
yonasketema <yonask.kitaw@gmail.com>
2023-05-23 15:34:56 +0300

    feat(logout):  with remove token from secureStore

--- hahu/app/screens/AccountScreen.js
+++ hahu/app/screens/AccountScreen.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import Screen from "../components/Screen";
+import authStorage from "../authStorage";
+import useAuth from "../hook/useAuth";
 
 function AccountScreen({}) {
+  const { setUser } = useAuth();
+  const handleLogOut = () => {
+    authStorage.removeTokens();
+    setUser(null);
+  };
   return (
     <Screen style={styles.container}>
       <View>
         <Text>AccountScreen</Text>
+        <Button title="Logout" onPress={handleLogOut} />
       </View>
     </Screen>
   );
