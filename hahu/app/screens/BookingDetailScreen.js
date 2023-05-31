0c6554a03ece1dd0fbbaa81b6560a947d19362a7
yonasketema <yonask.kitaw@gmail.com>
2023-05-31 16:11:33 +0300

    add date picker

--- hahu/app/screens/BookingDetailScreen.js
+++ hahu/app/screens/BookingDetailScreen.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { FontAwesome } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 import Screen from "../components/Screen";
 import ShowCard from "../components/ShowCard";
 
 function BookingDetailScreen({ route }) {
   const { trip } = route.params;
+
+  const [date, setDate] = useState(new Date());
+  const [showCalander, setShowCalander] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShowCalander(false);
+    setDate(currentDate);
+  };
+
+  const showDatepicker = () => {
+    setShowCalander(true);
+  };
 
   return (
     <Screen style={styles.container}>
@@ -17,12 +38,31 @@ function BookingDetailScreen({ route }) {
       />
 
       <View style={styles.booking_info_container}>
-        <Text style={styles.title}>Number Of People</Text>
-        <TextInput
-          style={styles.number_input}
-          keyboardType={"numeric"}
-          value="1"
-        />
+        {showCalander && (
+          <DateTimePicker
+            testID="datePicker"
+            value={date}
+            mode="date"
+            display="calendar"
+            onChange={onChange}
+          />
+        )}
+
+        <TouchableWithoutFeedback onPress={showDatepicker}>
+          <View style={styles.calendar_container}>
+            <FontAwesome name="calendar" size={18} color={"#000"} />
+            <Text style={styles.date_text}>{date.toDateString()} </Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <View style={styles.number_info}>
+          <Text style={styles.title}>Number Of People</Text>
+          <TextInput
+            style={styles.number_input}
+            keyboardType={"numeric"}
+            value="1"
+          />
+        </View>
       </View>
     </Screen>
   );
@@ -35,13 +75,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 9,
   },
   booking_info_container: {
+    paddingHorizontal: 24,
+    paddingVertical: 14,
+    backgroundColor: "#fff",
+  },
+  number_info: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "flex-start",
     gap: 21,
-    paddingVertical: 14,
-    paddingHorizontal: 24,
-    backgroundColor: "#fff",
   },
   title: {
     fontSize: 16,
@@ -52,11 +94,25 @@ const styles = StyleSheet.create({
     textAlign: "center",
     height: 40,
     width: 66,
-    borderRadius: 10,
+    borderRadius: 7,
     borderWidth: 2,
     borderColor: "#009688",
     fontSize: 19,
     fontWeight: "bold",
+  },
+  calendar_container: {
+    marginVertical: 17,
+    marginBottom: 25,
+    backgroundColor: "#eedd",
+    padding: 13,
+    paddingHorizontal: 12,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 7,
+    borderRadius: 7,
+  },
+  date_text: {
+    fontSize: 17,
   },
 });
 
