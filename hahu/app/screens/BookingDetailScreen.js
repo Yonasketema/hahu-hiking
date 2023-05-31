500e4d04a43d68cc185901145a6b442617cf7fa1
yonasketema <yonask.kitaw@gmail.com>
2023-05-31 15:28:55 +0300

    number of people input

--- hahu/app/screens/BookingDetailScreen.js
+++ hahu/app/screens/BookingDetailScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 import Screen from "../components/Screen";
 import ShowCard from "../components/ShowCard";
@@ -15,6 +15,15 @@ function BookingDetailScreen({ route }) {
         location={"ethiopia"}
         borderStyle="dashed"
       />
+
+      <View style={styles.booking_info_container}>
+        <Text style={styles.title}>Number Of People</Text>
+        <TextInput
+          style={styles.number_input}
+          keyboardType={"numeric"}
+          value="1"
+        />
+      </View>
     </Screen>
   );
 }
@@ -23,6 +32,31 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#eeddff",
     paddingTop: 45,
+    paddingHorizontal: 9,
+  },
+  booking_info_container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 21,
+    paddingVertical: 14,
+    paddingHorizontal: 24,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#999",
+  },
+  number_input: {
+    textAlign: "center",
+    height: 40,
+    width: 66,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#009688",
+    fontSize: 19,
+    fontWeight: "bold",
   },
 });
 
