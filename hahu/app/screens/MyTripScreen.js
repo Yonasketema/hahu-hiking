a1cb975d08a2a4c92921df8bf6cee01f9c92f144
yonasketema <yonask.kitaw@gmail.com>
2023-06-06 21:29:11 +0300

    add param trip data

--- hahu/app/screens/MyTripScreen.js
+++ hahu/app/screens/MyTripScreen.js
@@ -1,63 +1,57 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 import Screen from "../components/Screen";
+import apiClient from "../service/api-client";
+import ShowCard from "../components/ShowCard";
 
 function MyTripScreen() {
+  const [myTrip, setMyTrip] = useState();
+
+  useEffect(() => {
+    apiClient
+      .get("/api/mytrip")
+      .then((res) => setMyTrip(res.data))
+      .catch((err) => console.log("Error fetching myTrip", err));
+  }, []);
+
   return (
     <Screen style={styles.container}>
-      <View>
-        <Text
-          style={{
-            fontSize: 21,
-            fontWeight: "bold",
-            marginVertical: 5,
-            color: "#333",
-            marginHorizontal: 12,
-          }}
-        >
-          Your Next Trip
-        </Text>
-
-        <View
-          style={{
-            flexDirection: "row",
-            backgroundColor: "#fff",
-            padding: 12,
-            marginHorizontal: 12,
-          }}
-        >
-          <View>
-            <Image
-              style={{ height: 60, width: 70, marginRight: 10 }}
-              source={{
-                uri: "https://picsum.photos/200",
-              }}
-            ></Image>
-          </View>
-          <View>
-            <Text
-              style={{
-                fontSize: 14,
-                fontWeight: "500",
-                marginVertical: 5,
-                color: "#555",
-              }}
-            >
-              Guzo Adwa Hiking Group
-            </Text>
-            <View style={{ flexDirection: "row", gap: 5 }}>
-              <AntDesign name="star" size={17} color="#ffcd3c" />
-              <Text style={styles.rate_text}>4.8</Text>
-            </View>
-          </View>
-        </View>
-      </View>
+      <Text
+        style={{
+          fontSize: 19,
+          fontWeight: "500",
+          textAlign: "center",
+          marginBottom: 12,
+        }}
+      >
+        myTrip
+      </Text>
+      <FlatList
+        contentContainerStyle={{ marginHorizontal: 2 }}
+        showsHorizontalScrollIndicator={false}
+        data={myTrip}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { trip } }) => (
+          <ShowCard
+            title={trip.title}
+            image_uri={trip.image_uri}
+            location={"ethiopa"}
+          />
+        )}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  separator: {
+    width: "100%",
+    height: 1,
+    marginBottom: 5,
+    backgroundColor: "#eee",
+  },
+});
 
 export default MyTripScreen;
